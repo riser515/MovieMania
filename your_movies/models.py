9954ea4bf11bd977ce8cc366b43f646f7926e89f
Khushi Makhecha <makhechakhushi@gmail.com>
2023-03-26 10:46:17 -0400

    Model creation

--- your_movies/models.py
+++ your_movies/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=15)
+    email = models.CharField(max_length=60)
+    password = models.CharField(max_length=30)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+    
+class Movies(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    movie_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    release_date = models.DateField()
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.movie_name
+    
+class Reviews(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(Movies, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    comment = models.CharField(max_length=100)
+    is_deleted = models.BooleanField(default=False)
